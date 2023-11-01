159df4b9382d0a1dbe29b6b13334a2bca8429647
twkim <twkim7683@gmail.com>
2023-11-01 17:56:13 +0900

    비밀 정보 보호

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ readme
 npm run start로 실행
 
 mongodb -> db 사용, 좀 더 간편한 사용을 위해서 mongoose 사용 : npm install mongoose --save
+
+
+body parser - 클라이언트에서 가져오는 자료를 분석해서 출력해줌 : npm install body-parser --save
+
+postman - 서버로 데이터를 보내는 기능(api test 하는 툴)
+save 함수가 원래 인수를 받았는데 더 이상 받지 않게 되면서 비동기 함수를 다르게 적용해야 함
+
+nodemon - 소스의 변화를 자동으로 감지해서 자동으로 서버를 재시작 해줌, 새로고침은 해야 함 : npm install nodemon --save-dev (dev는 development mode)
+
+따로 개인 정보를 저장하기 위해서 gitignore을 사용해서 변수로 적어두기, 이때 배포 환경인지 개발환경인지에 따라서 변수가 달라지므로 모든 경우 고려해서 작성할 것
--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express'); //express module 가져오기
 const app = express();
 const port = 5000;
-
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://twkim7683:charls4740@boilerplate.13bahyi.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp')
-  .then(() => console.log('MongoDb Connected...'))
+
+const config = require('./config/key');
+
+app.use(bodyParser.urlencoded({extended: true})); //bodyparser를 통해서 분석할 수 있도록 함
+app.use(bodyParser.json()); //json 형태의 데이터를 분석할 수 있도록 함
+
+
+mongoose.connect(config.mongoURI)
+  .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World! 안녕하세요~~'));
+app.get('/', (req, res) => res.send('Hello World! 반갑습니다~ 안녕하세요~~')); //기본 route
+
+app.post('/register', async (req, res) => {
+    //회원 가입할 때 필요한 정보들을 client에서 가져오면 가져온 정보를 db에 넣기
+    const user = new User(req.body);
+    const result = await user.save()
+        .then(() => {
+            res.status(200).json({
+                success: true
+            });
+        })
+        .catch((err) => {
+            res.json({ success: false, err });
+        });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
